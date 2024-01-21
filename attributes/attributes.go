8329567d204c21bf7ba378061749c1c6451cda46
TANABE Ken-ichi <nabeken@tknetworks.org>
2024-01-21 22:48:59 +0900

    feat(attributes): use aws-sdk-go-v2

--- attributes/attributes.go
+++ attributes/attributes.go
@@ -4,35 +4,34 @@ package attributes
 import (
 	"strconv"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-// String returns dynamodb.AttributeValue for String.
-func String(v string) *dynamodb.AttributeValue {
-	return &dynamodb.AttributeValue{
-		S: aws.String(v),
+// String returns *types.AttributeValueMemberS for String.
+func String(v string) *types.AttributeValueMemberS {
+	return &types.AttributeValueMemberS{
+		Value: v,
 	}
 }
 
-// StringSet returns dynamodb.AttributeValue for String Set.
-func StringSet(ss []string) *dynamodb.AttributeValue {
-	return &dynamodb.AttributeValue{
-		SS: aws.StringSlice(ss),
+// StringSet returns *types.AttributeValueMemberSS for String Set.
+func StringSet(ss []string) *types.AttributeValueMemberSS {
+	return &types.AttributeValueMemberSS{
+		Value: ss,
 	}
 }
 
-// Number returns dynamodb.AttributeValue for Number.
-func Number(v int64) *dynamodb.AttributeValue {
+// Number returns *types.AttributeValueMemberN for Number.
+func Number(v int64) *types.AttributeValueMemberN {
 	n := strconv.FormatInt(v, 10)
-	return &dynamodb.AttributeValue{
-		N: &n,
+	return &types.AttributeValueMemberN{
+		Value: n,
 	}
 }
 
-// Binary returns dynamodb.AttributeValue for Binary.
-func Binary(b []byte) *dynamodb.AttributeValue {
-	return &dynamodb.AttributeValue{
-		B: b,
+// Binary returns *types.AttributeValueMemberB for Binary.
+func Binary(b []byte) *types.AttributeValueMemberB {
+	return &types.AttributeValueMemberB{
+		Value: b,
 	}
 }
